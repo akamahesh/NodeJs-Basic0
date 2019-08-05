086ed629b60c7211467be7e903996d6b4c7a87a3
akamahesh <mckay1718@gmail.com>
2019-08-06 04:18:26 +0530

    initial setup -express - routes - mongo

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const app = express();
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+//connect db
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API Running');
 });
+
+// Define routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.listen(PORT, () =>
   console.log(`Server started on port http://localhost:${PORT}`)
