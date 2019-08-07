d089eea7e381d6722bd12f07b8c5ab057c8cb38c
akamahesh <mckay1718@gmail.com>
2019-08-07 13:40:02 +0530

    base route

--- config/db.js
+++ config/db.js
@@ -5,7 +5,11 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
+var upload = multer({ dest: 'uploads/' });
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', upload.single('avatar'), (req, res, next) => {
+  console.log(' file ' + req.file);
+  console.log(req.body);
+  res.send(req.body);
+});
 
 module.exports = router;
